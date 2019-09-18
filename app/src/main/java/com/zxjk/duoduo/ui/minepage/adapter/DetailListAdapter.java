d9aecee97dbced5f883944df87473e85b5499c8a
J <574170873@qq.com>
2019-09-19 03:08:44 +0800

    [Version]1.1.0 [修改内容]增加流水条目 [新增功能]none [Note]none [TODO]none

--- app/src/main/java/com/zxjk/duoduo/ui/minepage/adapter/DetailListAdapter.java
+++ app/src/main/java/com/zxjk/duoduo/ui/minepage/adapter/DetailListAdapter.java
@@ -135,6 +135,30 @@ public class DetailListAdapter extends RecyclerView.Adapter<DetailListAdapter.Vi
                     ivItemDetaillist.setImageResource(R.drawable.ic_type7_12_13);
                     tvItemDetaillistName.setText("交易所手续费收取");
                     break;
+                case 14:
+                    ivItemDetaillist.setImageResource(R.drawable.ic_type11);
+                    tvItemDetaillistName.setText("签到");
+                    break;
+                case 15:
+                    ivItemDetaillist.setImageResource(R.drawable.ic_type11);
+                    tvItemDetaillistName.setText("实名认证");
+                    break;
+                case 16:
+                    ivItemDetaillist.setImageResource(R.drawable.ic_type11);
+                    tvItemDetaillistName.setText("注册奖励");
+                    break;
+                case 17:
+                    ivItemDetaillist.setImageResource(R.drawable.ic_type11);
+                    tvItemDetaillistName.setText("活动奖励");
+                    break;
+                case 18:
+                    ivItemDetaillist.setImageResource(R.drawable.ic_type11);
+                    tvItemDetaillistName.setText("活动奖励");
+                    break;
+                case 19:
+                    ivItemDetaillist.setImageResource(R.drawable.ic_type11);
+                    tvItemDetaillistName.setText("活动奖励");
+                    break;
                 default:
                     ivItemDetaillist.setImageResource(R.drawable.ic_detail_item_type5);
                     tvItemDetaillistName.setText("其他");
