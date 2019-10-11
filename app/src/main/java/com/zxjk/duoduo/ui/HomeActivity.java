9776981208d2ef74bb9f8aeb1eb13e64ea7700b1
J <574170873@qq.com>
2019-10-11 15:30:46 +0800

    [Version]1.2.0 [修改内容]1.1.0版本更新加入删除聊天信息判断 [新增功能]none [Note]none [TODO]none

--- app/src/main/java/com/zxjk/duoduo/ui/HomeActivity.java
+++ app/src/main/java/com/zxjk/duoduo/ui/HomeActivity.java
@@ -191,17 +191,6 @@ public class HomeActivity extends BaseActivity implements BottomNavigationBar.On
 
         initGreenDaoSession();
 
-        RongIM.getInstance().clearConversations(new RongIMClient.ResultCallback() {
-            @Override
-            public void onSuccess(Object o) {
-
-            }
-
-            @Override
-            public void onError(RongIMClient.ErrorCode errorCode) {
-
-            }
-        }, Conversation.ConversationType.GROUP, Conversation.ConversationType.PRIVATE);
     }
 
     @SuppressLint("CheckResult")
@@ -513,10 +502,29 @@ public class HomeActivity extends BaseActivity implements BottomNavigationBar.On
                                                 tvUpdate.setClickable(true);
                                                 tvUpdate.setText(R.string.dianjianzhuang);
                                                 tvUpdate.setOnClickListener(v1 -> {
-                                                    File file = new File(Utils.getApp().getCacheDir(), data.getVersion() + ".apk");// 设置路径
-                                                    Intent intent = installIntent(file.getPath());
-                                                    if (intent != null) {
-                                                        startActivity(intent);
+                                                    if (appVersionName.equals("1.1.0")) {
+                                                        RongIM.getInstance().clearConversations(new RongIMClient.ResultCallback() {
+                                                            @Override
+                                                            public void onSuccess(Object o) {
+                                                                File file = new File(Utils.getApp().getCacheDir(), data.getVersion() + ".apk");// 设置路径
+                                                                Intent intent = installIntent(file.getPath());
+                                                                if (intent != null) {
+                                                                    startActivity(intent);
+                                                                }
+                                                            }
+
+                                                            @Override
+                                                            public void onError(RongIMClient.ErrorCode errorCode) {
+                                                                tvUpdate.setText(R.string.dianjichongshi);
+                                                                ToastUtils.showShort(R.string.update_failure);
+                                                            }
+                                                        }, Conversation.ConversationType.GROUP, Conversation.ConversationType.PRIVATE);
+                                                    } else {
+                                                        File file = new File(Utils.getApp().getCacheDir(), data.getVersion() + ".apk");// 设置路径
+                                                        Intent intent = installIntent(file.getPath());
+                                                        if (intent != null) {
+                                                            startActivity(intent);
+                                                        }
                                                     }
                                                 });
                                                 tvUpdate.performClick();
