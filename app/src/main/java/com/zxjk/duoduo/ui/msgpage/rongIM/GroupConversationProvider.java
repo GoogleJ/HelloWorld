a478c4e105b5c10e346c39c04598de11cbef64cc
J <574170873@qq.com>
2019-12-11 10:00:11 +0800

    [Version]1.5.0 [修改内容] [新增功能]社群突显逻辑 70% [Note]none [TODO]none

--- app/src/main/java/com/zxjk/duoduo/ui/msgpage/rongIM/GroupConversationProvider.java
+++ app/src/main/java/com/zxjk/duoduo/ui/msgpage/rongIM/GroupConversationProvider.java
@@ -8,9 +8,6 @@ import android.widget.TextView;
 
 import com.zxjk.duoduo.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.rong.imkit.model.ConversationProviderTag;
 import io.rong.imkit.model.UIConversation;
 import io.rong.imkit.widget.provider.IContainerItemProvider;
@@ -23,41 +20,29 @@ public class GroupConversationProvider extends io.rong.imkit.widget.provider.Gro
     private int colorSocialSign;
     private int colorText;
 
-    private List<ViewHolder> holderList = new ArrayList<>();
-
     @Override
     public View newView(Context context, ViewGroup viewGroup) {
         if (colorSocialSign == 0) {
             colorSocialSign = Color.parseColor("#EC7A00");
             colorText = context.getColor(R.color.rc_text_color_primary);
         }
-        View view = super.newView(context, viewGroup);
-        ViewHolder holder = new ViewHolder();
-        holder.title = view.findViewById(io.rong.imkit.R.id.rc_conversation_title);
-        holder.tvSocialSign = view.findViewById(io.rong.imkit.R.id.tvSocialSign);
-        holderList.add(holder);
-
-        return view;
+        return super.newView(context, viewGroup);
     }
 
     @Override
     public void bindView(View view, int position, UIConversation data) {
         super.bindView(view, position, data);
-        ViewHolder holder = holderList.get(position);
-        if (holder != null) {
-            if (data.getUIConversationTitle().contains("おれは人间をやめるぞ！ジョジョ―――ッ!")) {
-                holder.tvSocialSign.setVisibility(View.VISIBLE);
-                holder.title.setTextColor(colorSocialSign);
-                holder.title.setText(data.getUIConversationTitle().replace("おれは人间をやめるぞ！ジョジョ―――ッ!", ""));
-            } else {
-                holder.tvSocialSign.setVisibility(View.GONE);
-                holder.title.setTextColor(colorText);
-            }
-        }
-    }
 
-    private class ViewHolder {
-        private TextView title;
-        private TextView tvSocialSign;
+        TextView title = view.findViewById(io.rong.imkit.R.id.rc_conversation_title);
+        TextView tvSocialSign = view.findViewById(io.rong.imkit.R.id.tvSocialSign);
+
+        if (data.getUIConversationTitle().contains("おれは人间をやめるぞ！ジョジョ―――ッ!")) {
+            tvSocialSign.setVisibility(View.VISIBLE);
+            title.setTextColor(colorSocialSign);
+            title.setText(data.getUIConversationTitle().replace("おれは人间をやめるぞ！ジョジョ―――ッ!", ""));
+        } else {
+            tvSocialSign.setVisibility(View.GONE);
+            title.setTextColor(colorText);
+        }
     }
 }
