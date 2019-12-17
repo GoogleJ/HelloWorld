3bdb52c2c782f5be1f7770507fde4e7aa3ee057f
J <574170873@qq.com>
2019-12-17 17:11:31 +0800

    [Version]1.5.1 [修改内容]userinfo逻辑优化 [新增功能]none [Note]none [TODO]none

--- app/src/main/java/com/zxjk/duoduo/ui/HomeActivity.java
+++ app/src/main/java/com/zxjk/duoduo/ui/HomeActivity.java
@@ -192,8 +192,20 @@ public class HomeActivity extends BaseActivity implements BottomNavigationBar.On
         }, true);
     }
 
+    @SuppressLint("CheckResult")
     private void registerRongMsgReceiver() {
         RongIM.setOnReceiveMessageListener((message, i) -> {
+            if (message != null && RongUserInfoManager.getInstance().getUserInfo(message.getSenderUserId()) == null) {
+                ServiceFactory.getInstance().getBaseService(Api.class)
+                        .getCustomerBasicInfoById(message.getSenderUserId())
+                        .compose(bindToLifecycle())
+                        .compose(RxSchedulers.normalTrans())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(user -> RongIM.getInstance().refreshUserInfoCache(new UserInfo(message.getSenderUserId(), user.getNick(), Uri.parse(user.getHeadPortrait()))),
+                                t -> {
+                                });
+            }
+
             Intent intent = new Intent(Constant.ACTION_BROADCAST2);
             intent.putExtra("msg", message);
             intent.putExtra("count", i);
