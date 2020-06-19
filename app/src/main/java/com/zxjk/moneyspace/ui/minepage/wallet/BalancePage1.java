656cc070d220a1d93fd8563799cbca09572a69d3
J <574170873@qq.com>
2020-06-19 11:38:17 +0800

    [Version]1.9.3 [修改内容]ssss [新增功能]none [Note]none [TODO]none

--- app/src/main/java/com/zxjk/moneyspace/ui/minepage/wallet/BalancePage1.java
+++ app/src/main/java/com/zxjk/moneyspace/ui/minepage/wallet/BalancePage1.java
@@ -20,13 +20,10 @@ public class BalancePage1 extends BaseFragment implements View.OnClickListener {
     private TextView tv1;
     private TextView tv2;
     private LinearLayout tv3;
-    private TextView tv4;
-    private TextView tv5;
     private TextView tv6;
     private TextView tv7;
     private TextView tv8;
     private TextView tv9;
-    private TextView tv12;
 
     @Nullable
     @Override
@@ -36,24 +33,20 @@ public class BalancePage1 extends BaseFragment implements View.OnClickListener {
         tv1 = rootView.findViewById(R.id.tv1);
         tv2 = rootView.findViewById(R.id.tv2);
         tv3 = rootView.findViewById(R.id.tv3);
-        tv4 = rootView.findViewById(R.id.tv4);
-        tv5 = rootView.findViewById(R.id.tv5);
+
         tv6 = rootView.findViewById(R.id.tv6);
         tv7 = rootView.findViewById(R.id.tv7);
         tv8 = rootView.findViewById(R.id.tv8);
         tv9 = rootView.findViewById(R.id.tv9);
-        tv12 = rootView.findViewById(R.id.tv12);
 
         tv1.setOnClickListener(this);
         tv2.setOnClickListener(this);
         tv3.setOnClickListener(this);
-        tv4.setOnClickListener(this);
-        tv5.setOnClickListener(this);
+
         tv6.setOnClickListener(this);
         tv7.setOnClickListener(this);
         tv8.setOnClickListener(this);
         tv9.setOnClickListener(this);
-        tv12.setOnClickListener(this);
 
         return rootView;
     }
@@ -78,25 +71,10 @@ public class BalancePage1 extends BaseFragment implements View.OnClickListener {
                 intent.putExtras(bundle);
                 getActivity().startActivity(intent);
                 break;
-            case R.id.tv4:
-                bundle.putString("url", "https://www.okex.me");
-                intent.putExtras(bundle);
-                getActivity().startActivity(intent);
-                break;
-            case R.id.tv5:
-                bundle.putString("url", "https://m.zb.live/");
-                intent.putExtras(bundle);
-                getActivity().startActivity(intent);
-                break;
             case R.id.tv6:
-                bundle.putString("url", "https://www.bitmex.com/");
-                intent.putExtras(bundle);
-                getActivity().startActivity(intent);
-                break;
             case R.id.tv7:
             case R.id.tv8:
             case R.id.tv9:
-            case R.id.tv12:
                 ToastUtils.showShort(R.string.developing);
                 break;
         }
