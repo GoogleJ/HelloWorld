47415affd4772c713aa53a045e35a8c54944f395
J <574170873@qq.com>
2020-07-30 11:26:42 +0800

    [Version]1.9.5 [修改内容]修复link无法点击的bug（无法长按文字气泡） [新增功能]none [Note]none [TODO]none

--- app/src/main/java/com/zxjk/duoduo/rongIM/provider/BurnTextMessageProvider.java
+++ app/src/main/java/com/zxjk/duoduo/rongIM/provider/BurnTextMessageProvider.java
@@ -1,6 +1,7 @@
 package com.zxjk.duoduo.rongIM.provider;
 
 import android.content.Context;
+import android.content.Intent;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
@@ -9,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
 
@@ -17,9 +17,13 @@ import com.blankj.utilcode.util.GsonUtils;
 import com.zxjk.duoduo.R;
 import com.zxjk.duoduo.bean.ConversationInfo;
 
+import io.rong.imkit.RongContext;
+import io.rong.imkit.RongIM;
 import io.rong.imkit.emoticon.AndroidEmoji;
 import io.rong.imkit.model.ProviderTag;
 import io.rong.imkit.model.UIMessage;
+import io.rong.imkit.widget.AutoLinkTextView;
+import io.rong.imkit.widget.LinkTextViewMovementMethod;
 import io.rong.imkit.widget.provider.IContainerItemProvider;
 import io.rong.imlib.model.Message;
 import io.rong.message.TextMessage;
@@ -76,6 +80,7 @@ public class BurnTextMessageProvider extends IContainerItemProvider.MessageProvi
         }
         BurnTextMessageProvider.ViewHolder holder = (BurnTextMessageProvider.ViewHolder) v.getTag();
         if (data.getMessageDirection() == Message.MessageDirection.SEND) {
+            holder.message.setLinkTextColor(ContextCompat.getColor(v.getContext(), R.color.text_msg_link_send));
             holder.message.setTextColor(ContextCompat.getColor(v.getContext(), R.color.white));
             holder.content.setBackgroundResource(io.rong.imkit.R.drawable.shape_rc_bubble_right);
             if (conversationInfo != null && conversationInfo.getMessageBurnTime() != -1) {
@@ -86,6 +91,7 @@ public class BurnTextMessageProvider extends IContainerItemProvider.MessageProvi
                 holder.ivFireLeft.setVisibility(View.INVISIBLE);
             }
         } else {
+            holder.message.setLinkTextColor(ContextCompat.getColor(v.getContext(), R.color.text_msg_link_receive));
             holder.message.setTextColor(ContextCompat.getColor(v.getContext(), R.color.new_textcolor1));
             holder.content.setBackgroundResource(io.rong.imkit.R.drawable.shape_rc_bubble_left);
             if (conversationInfo != null && conversationInfo.getMessageBurnTime() != -1) {
@@ -97,7 +103,7 @@ public class BurnTextMessageProvider extends IContainerItemProvider.MessageProvi
             }
         }
 
-        final TextView textView = holder.message;
+        final AutoLinkTextView textView = holder.message;
         if (data.getTextMessageContent() != null) {
             int len = data.getTextMessageContent().length();
             if (v.getHandler() != null && len > 500) {
@@ -107,11 +113,36 @@ public class BurnTextMessageProvider extends IContainerItemProvider.MessageProvi
             }
         }
 
-//        textView.stripUnderlines();
+        holder.message.setMovementMethod(new LinkTextViewMovementMethod(link -> {
+            String str = link.toLowerCase();
+            if (str.startsWith("http") || str.startsWith("https")) {
+                Intent intent = new Intent("io.rong.imkit.intent.action.webview");
+                intent.setPackage(v.getContext().getPackageName());
+                intent.putExtra("url", link);
+                v.getContext().startActivity(intent);
+                return true;
+            }
+
+            return false;
+        }));
+        holder.content.setOnLongClickListener(view -> {
+            RongIM.ConversationBehaviorListener listener = RongContext.getInstance().getConversationBehaviorListener();
+            RongIM.ConversationClickListener clickListener = RongContext.getInstance().getConversationClickListener();
+
+            boolean result = false;
+            if (listener != null) {
+                result = listener.onMessageLongClick(v.getContext(), holder.content, data.getMessage());
+            } else if (clickListener != null) {
+                result = clickListener.onMessageLongClick(v.getContext(), holder.content, data.getMessage());
+            }
+
+            return result;
+        });
+        textView.stripUnderlines();
     }
 
     private static class ViewHolder {
-        TextView message;
+        AutoLinkTextView message;
         ImageView ivFireLeft;
         ImageView ivFireRight;
         FrameLayout content;
